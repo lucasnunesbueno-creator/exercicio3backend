ddad855eda3d701f58f0c196f788d222e641fc42
lucasnunesbueno-creator <lucas.nunes.bueno@escola.pr.gov.br>
2026-03-09 08:49:31 -0300

    Add files via upload

--- exercicio.js
+++ exercicio.js
@@ -1,2 +1,17 @@
 console.log("escrevendo na linha de comando")
 console.log("exemplo")
+
+
+const dadosPessoa = {
+    "nome": "João Silva",
+    "idade": 17,
+    "hobbies": ["leitura", "natação", "videogame"],
+    "email:": "joao@email.com",
+    "endereco": {
+        "rua": "rua teste",
+        "numero": 15,
+        "cidade": "cidade teste"
+    }
+}
+
+console.log(dadosPessoa.hobbies);
